2bb1d1bb95acf977c7c08d6092c79c504b4da79f
USER <USER@DESKTOP-TRQG01D>
2022-11-11 19:39:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace For1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение переменной x=");
+            int x= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной n= ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double s = 0;
+            for (int k = 1; k <= n; k++)
+                s += Math.Pow(-1, k)* Math.Cos(k * x)/Math.Pow(k,2);
+            Console.WriteLine("s = {0}", s);
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
